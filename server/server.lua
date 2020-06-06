d50cec05cfe50bc87fc89654e38f8905dabb3e98
FlawwsX <55849405+FlawwsX@users.noreply.github.com>
2020-06-06 22:34:34 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,3 +1,6 @@
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
 local cooldown = false
 
 function CooldownCheck()
@@ -6,6 +9,22 @@ end
 
 function StartCooldown(time)
 	cooldown = true
-	Wait(temptime)
+	
+	if policecount > 5 then
+	    time = math.random(10, 30)
+	end
+	
+	Wait(time)
 	cooldown = false
 end
+
+--[[ Police Count ]] --
+
+function PoliceCount()
+	local policecount = 0
+	for i=1, #xPlayers, 1 do
+		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+		if xPlayer.job.name == 'police' then
+			policecount = policecount + 1
+		end
+	end
